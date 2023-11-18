5e7439fe797455fa8a134abf20447d88010db8f2
yuvraj-05828 <yuvraj05828@gmail.com>
2023-11-18 16:51:10 +0530

    updated code

--- score.js
+++ score.js
@@ -1,1 +1,13 @@
 // Iteration 8: Making scoreboard functional
+var urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams);
+var score = urlParams.get("score");
+
+var scoreBoard = document.getElementById("score-board");
+
+var playAgainButton = document.getElementById("play-again-button");
+
+scoreBoard.innerHTML = score;
+playAgainButton.onclick = (evt) => {
+  location.href = "./game.html";
+};
